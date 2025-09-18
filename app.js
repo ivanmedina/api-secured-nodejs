2e6113d2e8cfb86aee4e2805d2d58fc5964101ae
Ivan Medina <ivanmedina.contact@protonmail.com>
2025-09-17 18:03:01 -0600

    refactor(middlewares): refactored middlewares

--- app.js
+++ app.js
@@ -1,20 +1,12 @@
 const express = require('express');
-const rateLimit = require('express-rate-limit');
-const cors = require('cors');
 const multer = require('multer');
-const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
 const path = require('path');
 const https = require('https');
-const helmet = require('helmet');
-const hpp = require('hpp');
 
-const pool = require('./database/config');
 require('dotenv').config();
 
-const { hashPassword, verifyPassword, generateToken } = require('./utils/auth');
-const { authenticateToken,requireAdmin,requireOwnershipOrAdmin } = require('./middleware/auth');
-const antiSmuggling = require('./middleware/antiSmuggling');
+const { configureMiddlewares } = require('./config/middlewares');
 
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/users');
@@ -44,37 +36,7 @@ const handleMulterErrors = (err, req, res, next) => {
 
 // Middlewares
 
-// Rate limit
-
-const globalLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100, 
-  message: { 
-    error: 'Too many requests from this IP, please try again later.' 
-  },
-  standardHeaders: true,
-  legacyHeaders: false,
-});
-
-
-// CORS
-
-const corsOptions = {
-  origin: [
-    'http://localhost:3000',
-    'https://localhost:3000',
-    'localhost:3000'
-  ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true,
-};
-
-app.use(antiSmuggling);
-app.use(hpp());
-app.use(cors(corsOptions));
-app.use(globalLimiter); 
-app.use(helmet());
-app.use(express.json());
+configureMiddlewares(app);
 
 
 // Routes
---
+++ config/middlewares.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const rateLimit = require('express-rate-limit');
+const cors = require('cors');
+const helmet = require('helmet');
+const hpp = require('hpp');
+
+const antiSmuggling = require('../middleware/antiSmuggling');
+// const { handleMulterErrors } = require('../middleware/multerErrors');
+
+// Rate limit
+const globalLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100, 
+  message: { 
+    error: 'Too many requests from this IP, please try again later.' 
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+// CORS configuration
+const corsOptions = {
+  origin: [
+    'http://localhost:3000',
+    'https://localhost:3000',
+    'localhost:3000'
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+};
+
+const configureMiddlewares = (app) => {
+  app.use(antiSmuggling);
+  app.use(hpp());
+  app.use(cors(corsOptions));
+  app.use(globalLimiter); 
+  app.use(helmet());
+  app.use(express.json());
+//   app.use(handleMulterErrors);
+};
+
+module.exports = { configureMiddlewares };
